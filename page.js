a3f946c465f926e88399ffe6df85e3f85629ccae
NasikyeCynthia <cynthianasikye@gmail.com>
2024-11-07 22:50:15 +0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,16 @@
+import Title from  "./components/Title"
+import Content from "./components/Content"
+
+function Home(){
+    return (
+        <div>
+            <Title heading = "Cythias website yipii" />
+            <Content details = "This is my start of my react app baby"/>
+            <br/>
+            <p>God loves you sweet heart </p>
+
+        </div>
+    );
+
+}
+export default Home;
